d76366da13cc03cd9614e3d4d32a86bbaccfca2c
Benson Han <bensonhan@Bensons-Air.attlocal.net>
2023-06-22 19:13:57 -0700

    Changed object read from file to folder

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # vcf-qrcode
 
-Converts contact.vcf to a png QR code
+Converts contact.vcf to a png QR code. Put all contacts in .vcf file format into the contacts folder and then run:
+
+```python
+python vcf_to_qrcode.py
--- vcf_to_qrcode.py
+++ vcf_to_qrcode.py
@@ -1,14 +1,16 @@
 import qrcode
+import os
 
-# Read the VCF contact file
-with open('contact.vcf', 'r') as vcf_file:
-    vcf_data = vcf_file.read()
+for item in os.listdir('contacts'):
+    # Read the VCF contact file
+    with open('contacts/' + item, 'r') as vcf_file:
+        vcf_data = vcf_file.read()
 
-# Generate the QR code
-qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_M, box_size=10, border=4)
-qr.add_data(vcf_data)
-qr.make(fit=True)
+    # Generate the QR code
+    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_M, box_size=10, border=4)
+    qr.add_data(vcf_data)
+    qr.make(fit=True)
 
-# Save the QR code as an image
-qr_image = qr.make_image(fill_color="black", back_color="white")
-qr_image.save('contact_qr.png')
+    # Save the QR code as an image
+    qr_image = qr.make_image(fill_color="black", back_color="white")
+    qr_image.save(item.rstrip(".vcf") + ".png")
